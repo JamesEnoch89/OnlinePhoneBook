5646802bc2556869df34d062b27ec8f0a0161939
AaronStPete <AaronStPete@users.noreply.github.com>
2018-05-02 13:40:53 -0400

    added AuthContext

---
+++ OnlinePhoneBook/Context/AuthContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlinePhoneBook.Context
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}

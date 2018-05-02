61351590e04c2e55128175f40b014d22fc9dce41
jenoch <jenoch@JENOCH01>
2018-05-02 15:30:33 -0400

    SeedData

--- OnlinePhoneBook/Migrations/Configuration.cs
+++ OnlinePhoneBook/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace OnlinePhoneBook.Migrations
 {
+    using OnlinePhoneBook.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -15,18 +17,24 @@ namespace OnlinePhoneBook.Migrations
 
         protected override void Seed(OnlinePhoneBook.Context.AuthContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var contacts = new List<Contact>
+            {
+                new Contact {ContactID = 10248, Name = "Taiseer Joudeh", Address = "Amman", Email = "Amman@test.com", PhoneNumber = "5555555555", PreferredMethod = "Phone"},
+                new Contact {ContactID = 10249, Name = "Ahmad Hasan", Address = "Dubai", Email = "Dubai@test.com", PhoneNumber = "6666666666", PreferredMethod = "Email"},
+                new Contact {ContactID = 10250,Name = "Tamer Yaser", Address = "Jeddah", Email = "Jeddah@test.com", PhoneNumber = "7777777777", PreferredMethod = "Email"},
+                new Contact {ContactID = 10251,Name = "Lina Majed", Address = "Abu Dhabi", Email = "Abu Dhabi@test.com", PhoneNumber = "88888888", PreferredMethod = "Phone"},
+                new Contact {ContactID = 10252,Name = "Yasmeen Rami", Address = "Kuwait", Email = "Kuwait@test.com", PhoneNumber = "9999999999", PreferredMethod = "Phone"}
+            };
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            contacts.ForEach(contact =>
+            {
+                context.ContactList.AddOrUpdate(a => a.Name, contact);
+            });
+
+            context.SaveChanges();
+
+        }
         }
     }
-}
+
+

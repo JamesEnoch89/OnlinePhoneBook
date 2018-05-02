8e1295a198a4615a7d330e3180bbbcfd70d0d8d5
AaronStPete <AaronStPete@users.noreply.github.com>
2018-05-02 14:42:25 -0400

    create user and /token are functional

--- OnlinePhoneBook/Startup.cs
+++ OnlinePhoneBook/Startup.cs
@@ -17,6 +17,8 @@ namespace OnlinePhoneBook
         public void Configuration(IAppBuilder app)
         {
             HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+
             WebApiConfig.Register(config);
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(config);

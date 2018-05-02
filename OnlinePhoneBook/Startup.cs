9dd66df8d79581fd34ad020e4d5f52a79ecae627
AaronStPete <AaronStPete@users.noreply.github.com>
2018-05-02 14:03:48 -0400

    added to startup

--- OnlinePhoneBook/Startup.cs
+++ OnlinePhoneBook/Startup.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
- 
+
 [assembly: OwinStartup(typeof(OnlinePhoneBook.Startup))]
 namespace OnlinePhoneBook
 
@@ -19,5 +20,20 @@ namespace OnlinePhoneBook
             app.UseWebApi(config);
         }
 
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
     }
 }
